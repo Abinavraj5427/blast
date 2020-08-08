db96d590a8b5d2692421d088ae1dc8b94de9699b
Brian-Taylor8 <bpt@berkeley.edu>
2020-08-08 06:00:51 -0700

    Working automated FB login
    Working FB login, having some trouble with the post

---
+++ python/facebook_login_post.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+PATH = "/Users/briantaylor/Desktop/important/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.facebook.com")
+
+username = driver.find_element_by_id("email")
+username.send_keys("bptincali@gmail.com")
+
+password = driver.find_element_by_id('pass')
+password.send_keys("1234567a")
+password.send_keys(Keys.ENTER)
