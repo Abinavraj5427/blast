3f5339a078382fcc147db6d6bfb747a5502bd225
Brian-Taylor8 <bpt@berkeley.edu>
2020-08-08 16:30:49 -0700

    Update twitter_post_data.py

--- python/twitter_post_data.py
+++ python/twitter_post_data.py
@@ -3,10 +3,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup as bs
 
 import time
 
-PATH = "/Users/Wish/Downloads/chromedriver"
+PATH = "/Users/briantaylor/Desktop/important/chromedriver"
 
 driver = webdriver.Chrome(PATH)
 
@@ -23,7 +24,17 @@ button = driver.find_element_by_xpath("/html/body/div/div/div/div[2]/main/div/di
 button.click()
 
 
+
 #time.sleep(5) 
+url = 'https://twitter.com/CollegeBoard/status/1291750806920798209'
+driver.get(url)
+
+likes = driver.find_element_by_css_selector(".css-1dbjc4n")
+print(likes.text)
+
+
+
+"""
 urls = ['https://twitter.com/throwaway394959/status/1292065983146758145',
 	"https://twitter.com/boswellhs/status/1291830495551074305",
 	"https://twitter.com/CollegeBoard/status/1291750806920798209"]
@@ -32,7 +43,7 @@ for url in urls:
 	#total_number =  driver.find_element_by_css_selector(".css-16my406")
 	#print(total_number.text)
 
-
+"""
 
 
 
