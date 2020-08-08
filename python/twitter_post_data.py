2b95a60bc767d8d6d8daa1b3216de7479a461c92
Brian-Taylor8 <bpt@berkeley.edu>
2020-08-08 10:51:31 -0700

    Update twitter_post_data.py

--- python/twitter_post_data.py
+++ python/twitter_post_data.py
@@ -7,12 +7,14 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 PATH = "/Users/Wish/Downloads/chromedriver"
+
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.twitter.com/login")
 
+driver.get("https://www.twitter.com/login")
+time.sleep(1)
 username_login = driver.find_element_by_name("session[username_or_email]")
-username_login.send_keys("throwaway394959")
+username_login.send_keys("throwaway3949596@gmail.com")
 
 password_login = driver.find_element_by_name("session[password]")
 password_login.send_keys("1234567a")
@@ -20,8 +22,8 @@ password_login.send_keys("1234567a")
 button = driver.find_element_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div/div")
 button.click()
 
-#time.sleep(5)
 
+#time.sleep(5) 
 urls = ['https://twitter.com/throwaway394959/status/1292065983146758145',
 	"https://twitter.com/boswellhs/status/1291830495551074305",
 	"https://twitter.com/CollegeBoard/status/1291750806920798209"]
@@ -29,3 +31,14 @@ for url in urls:
 	driver.get(url)
 	#total_number =  driver.find_element_by_css_selector(".css-16my406")
 	#print(total_number.text)
+
+
+
+
+
+
+
+
+
+
+
