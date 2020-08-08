1f0a2e7f0753536157ac6cf72ef15d7a029f5d7b
Brian-Taylor8 <bpt@berkeley.edu>
2020-08-07 23:34:17 -0700

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,3 +1,18 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
+PATH = "/Users/briantaylor/Desktop/important/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.reddit.com/login/")
+
+username_login = driver.find_element_by_id('loginUsername')
+username_login.send_keys("throwaway3949596")
+
+password_login = driver.find_element_by_id("loginPassword")
+password_login.send_keys("1234567a")
 
