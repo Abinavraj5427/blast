ba414aa06944c6e2a1b404e8efdcb0318016d077
Brian-Taylor8 <bpt@berkeley.edu>
2020-08-08 21:06:43 -0700

    Update twitter_post.py

--- python/twitter_post.py
+++ python/twitter_post.py
@@ -6,29 +6,32 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
-PATH = "/Users/briantaylor/Desktop/important/chromedriver"
+def login_and_post_twitter(username, password, message):
+	PATH = "/Users/briantaylor/Desktop/important/chromedriver"
 
-driver = webdriver.Chrome(PATH)
+	driver = webdriver.Chrome(PATH)
 
+	driver.get("https://www.twitter.com/login")
+	time.sleep(1)
+	username_login = driver.find_element_by_name("session[username_or_email]")
+	username_login.send_keys(username)
 
-driver.get("https://www.twitter.com/login")
-time.sleep(1)
-username_login = driver.find_element_by_name("session[username_or_email]")
-username_login.send_keys("throwaway3949596@gmail.com")
+	password_login = driver.find_element_by_name("session[password]")
+	password_login.send_keys(password)
 
-password_login = driver.find_element_by_name("session[password]")
-password_login.send_keys("1234567a")
-
-button = driver.find_element_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div/div")
-button.click()
-
-
-body = driver.find_element_by_css_selector(".public-DraftStyleDefault-block")
-body.send_keys("Test message #7")
-body.send_keys(Keys.COMMAND, Keys.ENTER)
+	button = driver.find_element_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div/div")
+	button.click()
 
 
+	body = driver.find_element_by_css_selector(".public-DraftStyleDefault-block")
+	body.send_keys(message)
+	body.send_keys(Keys.COMMAND, Keys.ENTER)
+	time.sleep(100)
 
+
+
+if __name__ == '__main__':
+	login_and_post_twitter("throwaway3949596@gmail.com", "1234567a","Test message #9")
 
 
 
